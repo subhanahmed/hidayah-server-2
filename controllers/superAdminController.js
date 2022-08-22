7d35c6c7e3b16ac493fd676ef402a5d75fa246ab
subhanahmed <30408854+subhanahmed@users.noreply.github.com>
2022-08-22 01:53:06 -0700

    added superAdmin Controller

---
+++ controllers/superAdminController.js
@@ -0,0 +1,23 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel.js')
+
+// @desc    Get all Students
+// @route   GET /api/superAdmin/students
+// @access  Private/SuperAdmin
+const students = asyncHandler(async (req, res) => {
+  const users = await User.find({ 'isStudent': true })
+  res.json(users)
+})
+
+// @desc    Get all Faculties
+// @route   GET /api/superAdmin/faculties
+// @access  Private/SuperAdmin
+const faculties = asyncHandler(async (req, res) => {
+  const users = await User.find({ 'isTeacher': true })
+  res.json(users)
+})
+
+module.exports = {
+  students,
+  faculties
+} 
