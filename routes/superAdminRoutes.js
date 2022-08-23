749fbc89e548d4a8913e76f4def6ec4f5846a5f8
subhanahmed <30408854+subhanahmed@users.noreply.github.com>
2022-08-23 01:45:22 -0700

    Update superAdminRoutes.js

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router()
 const {
   students,
-  faculties
+  faculties,
+  studentsCount,
+  facultiesCount
 } = require('../controllers/superAdminController');
 const protect = require('../middleware/authMiddleware.js')
 
 router.route('/students').get(students)
 router.route('/faculties').get(faculties)
+router.route('/students/count').get(studentsCount)
+router.route('/faculties/count').get(facultiesCount)
 
 module.exports = router
