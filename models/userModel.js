fd41559e14092d0c9f9d170cc9b3286fb333350a
subhanahmed <30408854+subhanahmed@users.noreply.github.com>
2022-08-29 01:42:58 -0700

    Update rollNumber

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const userSchema = mongoose.Schema(
     {
+        rollNumber: {
+            type: 'string',
+            default: ''
+        },
         firstName:{
             type: 'string',
             default: ''
